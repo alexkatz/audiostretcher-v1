996d1621602bf9928d624674a3ff7e77506a21f2
alexkatz <alexanderikatz@gmail.com>
2018-01-14 08:44:47 -0500

    youtube url on welcome page, busy indicator

--- client/src/home/Welcome.tsx
+++ client/src/home/Welcome.tsx
@@ -1,15 +1,19 @@
 import * as React from 'react';
+import Popover, { ArrowContainer } from 'react-tiny-popover';
 import { Style } from '../shared/styles';
 import { Color } from '../shared/colors';
 import { Constant } from '../shared/constants';
+import { BusyIndicator } from '../shared/busyIndicator';
 
 interface WelcomeProps {
   width: number;
   onLoadUrl?(urlText: string);
+  isBusy?: boolean;
 }
 
 interface WelcomeState {
   urlText: string;
+  isPopoverOpen: boolean;
 }
 
 class Welcome extends React.Component<WelcomeProps, WelcomeState> {
@@ -17,12 +21,19 @@ class Welcome extends React.Component<WelcomeProps, WelcomeState> {
     super(props);
     this.state = {
       urlText: '',
+      isPopoverOpen: false,
     };
   }
 
+  public componentDidUpdate(prevProps: WelcomeProps, prevState: WelcomeState) {
+    if (this.state.urlText !== prevState.urlText) {
+      this.setState({ isPopoverOpen: this.state.urlText.length > 0 && !Constant.IS_YOUTUBE_URL(this.state.urlText) });
+    }
+  }
+
   public render() {
-    const { width, onLoadUrl } = this.props;
-    const { urlText } = this.state;
+    const { width, onLoadUrl, isBusy } = this.props;
+    const { urlText, isPopoverOpen } = this.state;
     return (
       <div
         style={{
@@ -64,50 +75,82 @@ class Welcome extends React.Component<WelcomeProps, WelcomeState> {
             }}
             children={'youtube url'}
           />
-          <div // input and button container
-            style={{
-              display: 'flex',
-              height: 100,
-              ...Style.INNER_BLUE_BOX_SHADOW,
-              ...Style.BORDER_RADIUS,
-            }}
+          <Popover
+            isOpen={isPopoverOpen}
+            content={({ targetRect, popoverRect }) => (
+              <ArrowContainer
+                arrowColor={Color.MID_BLUE}
+                position={'top'}
+                targetRect={targetRect}
+                popoverRect={popoverRect}
+              >
+                <div
+                  style={{
+                    padding: Constant.PADDING,
+                    backgroundColor: Color.MID_BLUE,
+                    color: Color.LIGHT_BLUE,
+                    ...Style.BORDER_RADIUS,
+                  }}
+                >
+                  make sure this link is to a youtube video!
+                </div>
+              </ArrowContainer>
+            )}
           >
-            <input
+            <div // input and button container
               style={{
-                minHeight: Constant.SLIDER_WIDTH,
-                outline: 'none',
-                backgroundColor: 'transparent',
-                border: 'none',
-                padding: Constant.PADDING,
-                fontSize: Constant.FONT_SIZE.LARGE,
-                color: Color.MID_BLUE,
-                flex: '1 1 auto',
-              }}
-              type={'text'}
-              value={urlText}
-              onChange={e => this.setState({ urlText: e.target.value })}
-              onClick={e => e.stopPropagation()}
-            />
-            <button
-              onClick={e => {
-                e.stopPropagation();
-                onLoadUrl(urlText);
-              }}
-              style={{
-                fontSize: Constant.FONT_SIZE.REGULAR,
                 display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                border: 'none',
-                outline: 'none',
-                color: Color.LIGHT_BLUE,
-                backgroundColor: Color.MID_BLUE,
+                height: 100,
+                ...Style.INNER_BLUE_BOX_SHADOW,
                 ...Style.BORDER_RADIUS,
-                cursor: 'pointer',
               }}
-              children={'get audio'}
-            />
-          </div>
+            >
+              <input
+                style={{
+                  minHeight: Constant.SLIDER_WIDTH,
+                  outline: 'none',
+                  backgroundColor: 'transparent',
+                  border: 'none',
+                  padding: Constant.PADDING,
+                  fontSize: Constant.FONT_SIZE.LARGE,
+                  color: Color.MID_BLUE,
+                  flex: '1 1 auto',
+                }}
+                type={'text'}
+                value={urlText}
+                onChange={e => this.setState({ urlText: e.target.value })}
+                onClick={e => e.stopPropagation()}
+              />
+              <button
+                onClick={e => {
+                  e.stopPropagation();
+                  if (!isBusy) {
+                    onLoadUrl(urlText);
+                  }
+                }}
+                style={{
+                  fontSize: Constant.FONT_SIZE.REGULAR,
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  border: 'none',
+                  outline: 'none',
+                  color: Color.LIGHT_BLUE,
+                  backgroundColor: Color.MID_BLUE,
+                  ...Style.BORDER_RADIUS,
+                  cursor: isBusy ? 'cursor' : 'pointer',
+                  width: 120,
+                }}
+              >
+                {isBusy && (
+                  <BusyIndicator />
+                )}
+                {!isBusy && (
+                  <span children={'get audio'} />
+                )}
+              </button>
+            </div>
+          </Popover>
         </div>
         <div
           style={{
